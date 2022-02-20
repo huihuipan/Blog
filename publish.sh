4560d1ca3eb77caa270b9a11697c63ce01cbc8bd
huihuipan <huihuipan163@163.com>
2022-02-20 13:14:18 +0800

    publish

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 # /bin/bash
 
-cd C:/$/Blog
+cd ~/Blog
 
 git add .
 
@@ -10,24 +10,24 @@ git push origin main
 
 echo '已提交Blog'
 
-rm C:/$/hexo/source/_posts/*
+rm C:~/hexo_for_blog/source/_posts/*
 
-cp C:/$/Blog/Javascript/*.md C:/$/hexo/source/_posts
-cp C:/$/Blog/Java/*.md C:/$/hexo/source/_posts
-cp C:/$/Blog/Linux/*.md C:/$/hexo/source/_posts
-cp C:/$/Blog/DB/*.md C:/$/hexo/source/_posts
-cp C:/$/Blog/Docker/*.md C:/$/hexo/source/_posts
-cp C:/$/Blog/Nginx/*.md C:/$/hexo/source/_posts
-cp C:/$/Blog/Typescript/*.md C:/$/hexo/source/_posts
-cp C:/$/Blog/设计模式/*.md C:/$/hexo/source/_posts
-cp C:/$/Blog/数据结构与算法/*.md C:/$/hexo/source/_posts
+cp ~/Blog/Javascript/*.md ~/hexo_for_blog/source/_posts
+cp ~/Blog/Java/*.md ~/hexo_for_blog/source/_posts
+cp ~/Blog/Linux/*.md ~/hexo_for_blog/source/_posts
+cp ~/Blog/DB/*.md ~/hexo_for_blog/source/_posts
+cp ~/Blog/Docker/*.md ~/hexo_for_blog/source/_posts
+cp ~/Blog/Nginx/*.md ~/hexo_for_blog/source/_posts
+cp ~/Blog/Typescript/*.md ~/hexo_for_blog/source/_posts
+cp ~/Blog/设计模式/*.md ~/hexo_for_blog/source/_posts
+cp ~/Blog/数据结构与算法/*.md ~/hexo_for_blog/source/_posts
 
 echo '复制文章'
 
 
 echo '切换目录至hexo'
 
-cd C:/$/hexo
+cd ~/hexo_for_blog
 
 echo '构建'
 
@@ -35,7 +35,7 @@ npm run build
 
 echo '构建成功'
 
-cd C:/$/hexo/public
+cd ~/hexo_for_blog/public
 
 git add .
 
